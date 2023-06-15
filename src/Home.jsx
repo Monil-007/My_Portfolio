dd75e9a2d4046c9d5a31f7162112723ccef2a229
Monil Gandhi <gandhi.monil007@gmail.com>
2023-06-15 13:15:34 +0530

    code pushed

---
+++ src/Home.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import HeroSection from "./components/HeroSection";
+import { useGlobalContext } from "./context";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import { homeproject, resume } from "./components/Reusable";
+
+const Home = () => {
+  const { updateHomePage } = useGlobalContext();
+
+  useEffect(() => updateHomePage(), []);
+
+
+  return (
+    <>
+      <HeroSection link="/resume" btnName="resume" />
+      <Experience />
+
+      <Projects homeproject={homeproject} />
+      <Contact />
+    </>
+  );
+};
+
+
+
+export default Home;
+
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
+import { useGlobalContext } from "../context";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 1000,
+});
+
+const HeroSection = (props) => {
+  const { name } = useGlobalContext();
+
+
+
+  return (
+    <Wrapper>
+      <div className="container grid grid-two-column">
+        <div className="section-hero-data" data-aos="fade-right">
+          <h3>Hii! I am</h3>
+          <h1 className="hero-heading">{name}</h1>
+          <p className="hero-para">
+            I,m a MERN Stack developer and a huge Coffee lover!!
+            <br />
+            {props.description}
+          </p>
+          <NavLink to={props.link}>
+            <Button className="btn hireme-btn">
+              {props.btnName}
+            </Button>
+          </NavLink>
+        </div>
+
+        {/* for image  */}
+        <div className="section-hero-image" data-aos="fade-left">
+          <picture>
+            <img
+              src="https://i.postimg.cc/KzqCKG0H/Hire-Web-Developers.png"
+              alt="hero image"
+              className="hero-img "
+            />
+          </picture>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  overflow-x: hidden;
+  padding: 9rem 0;
+
+  .section-hero-data {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .btn {
+    max-width: 16rem;
+  }
+
+  .hero-top-data {
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 1.5rem;
+    font-family: 'Hanuman', serif;
+    color: ${({ theme }) => theme.colors.helper};
+  }
+
+  .hero-heading {
+    text-transform: uppercase;
+    font-size: 6.4rem;
+  }
+
+  .hero-para {
+    margin-top: 1.5rem;
+    margin-bottom: 3.4rem;
+    max-width: 60rem;
+    color: rgb(0,0,0);
+    font-weight: 600;
+    font-family: 'Hanuman', serif;
+  }
+
+  .section-hero-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  picture {
+    text-align: center;
+  }
+
+  .hero-img {
+    max-width: 80%;
+  }
+  .hireme-btn{
+    font-size: 20px
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 7.2rem;
+    }
+  }
+`;
+
+export default HeroSection;
